46c0278120e92e0237a40e2102b3d9fee3971964
Richard Tran <RichardTran93@gmail.com>
2017-04-19 05:37:36 +0000

    updated on 2017-04-19 05:37:36

--- update.sh
+++ update.sh
@@ -15,3 +15,6 @@ rm adblock.unsorted adblock.sorted
 git add *
 git commit -a -m "updated on `date +'%Y-%m-%d %H:%M:%S'`";
 git push
+
+# remove time stamp
+head -n -1 adblock.txt > temp.txt ; mv temp.txt adblock.txt
