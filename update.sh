4b5b5437c453340f76bef8c4cbbb5b32e889619c
Richard Tran <RichardTran93@gmail.com>
2017-04-23 01:51:22 -0700

    updated on 2017-04-23 01:51:22

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 # download adblock lists
-curl -s -L https://easylist.to/easylist/easylist.txt https://fanboy.co.nz/enhancedstats.txt https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt https://raw.githubusercontent.com/gioxx/xfiles/master/facebook.txt https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt https://fanboy.co.nz/fanboy-antifacebook.txt https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt https://www.malwaredomainlist.com/hostslist/hosts.txt https://easylist-downloads.adblockplus.org/easyprivacy+easylist.txt https://adaway.org/hosts.txt https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer-filters.txt https://raw.githubusercontent.com/Yhonay/antipopads/master/popads.txt https://raw.githubusercontent.com/IDKwhattoputhere/uBlock-Filters-Plus/master/uBlock-Filters-Plus.txt https://easylist-downloads.adblockplus.org/adwarefilters.txt https://easylist-downloads.adblockplus.org/easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt > adblock.unsorted
-
+curl -s -L https://adaway.org/hosts.txt > adblock.unsorted
 # look for:  ||domain.tld^
 sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
 
